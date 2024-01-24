0b55aa22ff91529351f591623045f02fc5ef0f4c
TaekyungHa <86tkstar@gmail.com>
2024-01-24 18:31:46 +0900

    first init

--- frontend/src/__test__/App.test.tsx
+++ frontend/src/__test__/App.test.tsx
@@ -3,6 +3,5 @@ import App from "../App.tsx";
 
 test("renders hello message", () => {
   render(<App />);
-  expect(screen.getByText(/Vite \+ React \+ TypeScript/i)).toBeInTheDocument();
-  expect(screen.getByText(/Jest/i)).toBeInTheDocument();
+  expect(screen.getByText(/Super Compose Test/i)).toBeInTheDocument();
 });
