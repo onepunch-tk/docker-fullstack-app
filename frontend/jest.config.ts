8af3104235f6477297f897d96ef9bc4ef92f329f
TaekyungHa <86tkstar@gmail.com>
2024-01-24 14:43:51 +0900

    first init

---
+++ frontend/jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  verbose: true,
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  moduleNameMapper: {
+    // SVG 파일을 모의 객체로 리다이렉트
+    "\\.svg$": "<rootDir>/src/__mocks__/svgMock.cjs",
+    "\\.css$": "<rootDir>/src/__mocks__/styleMock.cjs",
+  },
+};
+
+export default config;
