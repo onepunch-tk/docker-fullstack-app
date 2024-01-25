0c17ce37143f86deb63f02dd350bacd7fc0e3b7a
TaekyungHa <86tkstar@gmail.com>
2024-01-25 15:52:17 +0900

    add secret manager

--- .platform/confighooks/prebuild/01login.sh
+++ .platform/confighooks/prebuild/01login.sh
@@ -1,2 +1,2 @@
-USER=/opt/elasticbeanstalk/bin/get-config environment -k DOCK_HUB_USER
-/opt/elasticbeanstalk/bin/get-config environment -k DOCK_HUB_TOKEN | docker login -u $USER --password-stdin
+USER=/opt/elasticbeanstalk/bin/get-config environment -k DOCKER_USER
+/opt/elasticbeanstalk/bin/get-config environment -k DOCKER_TOKEN | docker login -u $USER --password-stdin
